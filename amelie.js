eeb5c3c143f0e6a22f6a4fbd0246621d01e3972a
Dimitris Psaropoulos <info@harby.me>
2014-03-01 14:54:13 +0100

    first commit

---
+++ amelie.js
@@ -0,0 +1,25 @@
+var delegateEventSplitter = /^(\S+)\s*(.*)$/;
+var delegateEvents = function(obj) {
+  if (obj.events !== undefined) {
+    var $el = $(obj.el || document);
+    for (var key in obj.events) {
+      var match = key.match(delegateEventSplitter);
+      var eventName = match[1], selector = match[2];
+      var method = obj[obj.events[key]];
+
+      if (selector === '') {
+        $el.on(eventName, method);
+      } else {
+        $el.on(eventName, selector, method);
+      }
+    }
+  }
+};
+function Amelie() {}
+Amelie.extend = function(obj) {
+  delegateEvents(obj);
+  if (obj.initialize !== undefined) {
+    obj.initialize();
+  }
+  return $.extend({}, obj, Class);
+};
